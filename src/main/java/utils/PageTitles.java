26fc7456b4935d771bf55e5d7cc4cb670160589f
Bartosz Kita <bartosz.kita@gmail.com>
2021-04-20 14:05:52 +0200

    first commit

---
+++ src/main/java/utils/PageTitles.java
@@ -0,0 +1,10 @@
+package utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageTitles {
+
+    public static final String HOME_PAGE_TITLE = "My Store";
+}
---
+++ src/test/java/tests/HomePageTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.PopularItemsPage;
+import url.Url;
+import utils.PageTitles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HomePageTest extends BaseTest {
+
+    private PopularItemsPage popularItemsPage;
+
+    @BeforeEach
+    public void setupTest() {
+        driver = new ChromeDriver();
+        driver.get(Url.BASE_URL);
+        assertThat(driver.getTitle()).isEqualTo(PageTitles.HOME_PAGE_TITLE);
+
+        popularItemsPage = new PopularItemsPage(driver);
+    }
+
+    @Test
+    void shouldSeeSevenPopularItemsOnHomePage() {
+        List<String> productNames = popularItemsPage.getProductNames();
+        assertThat(productNames.size()).isEqualTo(7);
+    }
+}
